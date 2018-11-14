107888c7004d45c144a28499e65580e2f7b8b2b5
Luis Zabala <luis.zabala076@gmail.com>
2018-11-14 19:47:40 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,7 @@
 var playlist = {
   'Machine Head': 'Impirium',
   'Dimmu Borgir': 'Purtania'
+<<<<<<< HEAD
 }
 
 function updatePlaylist (playlist, artist, song){
@@ -11,4 +12,15 @@ function updatePlaylist (playlist, artist, song){
 function removeFromPlaylist (playlist, artist) {
   delete playlist[artist];
   return playlist;
+=======
+};
+
+function updatePlaylist (playlist, artist, song){
+  playlist[artist] = song;
+  
+}
+
+function removeFromPlaylist (playlist, artist) {
+  delete artist.playlist;
+>>>>>>> c235e1af88a6c4d70feb0063ae5edf084d49ae6b
 }
